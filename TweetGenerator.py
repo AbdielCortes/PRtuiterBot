9973b22bf47c86c06a3da13a76eb3c1d721a17ef
Abdiel Cortés <zurken@Abdiels-MacBook-Pro.local>
2020-08-25 19:54:58 -0400

    added more phrases and sentences to the tweet generator

--- TweetGenerator.py
+++ TweetGenerator.py
@@ -5,13 +5,17 @@ import random
 
 pharases = ["jevos y panas", "la bicha y el caco", "una baby toxica", "un airbnb en el west",
            "esto y un blunt", "dividir los votos", "estadity", "PNPPD", "el bipartidismo",
-           "Lugaro vs Dalmau", "independencia", "Ponce", "empanadilla vs pastelillo", "Bad Bunny",
-           "un esquema de piramide", "la colonia", "estadidad", "Cuba y Venezuela", "socialismo",
-           "comunismo", "pelus de la UPR"]
+           "Lugaro vs Dalmau", "la independencia", "Ponce", "empanadilla vs pastelillo", "Bad Bunny",
+           "un esquema de piramide", "la colonia", "la estadidad", "Cuba y Venezuela", "el socialismo",
+           "el comunismo", "los pelus de la UPR", "la UPR", "Lugaro", "Dalmau", "el PNP"]
 
 sentences = ["Somos %s, pero no estan listos para esa conversacion", "Quisiera que fueramos %s",
              "Gracias a #Forex, logre tener %s", "Odio que en Puerto Rico lo unico que hay es %s",
-             "Me cago mil veces en to los pendejos que lo unico que hablan es de %s"]
+             "Me cago mil veces en to los pendejos que lo unico que hablan es de %s",
+             "Cabron por que no entiendes que es %s, NO es un esquema de piramide",
+             "Hablas de %s como si tuiverans un cabron doctorado en eso",
+             "El problema de Puerto Rico es %s",
+             "Si llego a ver un tweet mas de %s, me voy pal carajo de esta app del pajaro azul"]
 
 def generate_tweet():
     # pick a phrase by generation a random index
